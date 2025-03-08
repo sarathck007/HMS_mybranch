c77408a36e7e722b12b2f75064f71cbcfdcabf04
sarathchandran karintha <sarathck84@gmail.com>
2025-03-07 20:24:04 -0500

    updated packages test

---
+++ HotelManagementSystem/src/test/java/com/group3/hotelsystem/HotelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.hotelsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
